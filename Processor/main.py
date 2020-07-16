130f0b15f027b8567242d55058759e7b39cd3377
aref <arefsayareh@gmail.com>
2020-07-16 21:38:52 +0430

    change test1

--- Processor/main.py
+++ Processor/main.py
@@ -1,7 +1,29 @@
 from Components.Register import Register
+from flipflop.d import D_FlipFlop
+from gate.and_gate import And
+from gate.input_gate import Input
+from gate.not_gate import Not
+from gate.or_gate import Or
+from gate.xor_gate import Xor
+from latch.d import D_Latch
+from multiplexer.mux2x1 import Mux2x1
+from multiplexer.mux4x2 import Mux4x2
 from signals.signal import Signal
+
 from random import randint
-from gate import one_gate, zero_gate
+
+
+def turn_off_debug(every_thing=False):
+    And.DEBUGMODE = every_thing
+    Or.DEBUGMODE = every_thing
+    Xor.DEBUGMODE = every_thing
+    D_FlipFlop.DEBUGMODE = every_thing
+    D_Latch.DEBUGMODE = every_thing
+    Not.DEBUGMODE = every_thing
+    Mux2x1.DEBUGMODE = every_thing
+    Mux4x2.DEBUGMODE = every_thing
+    Signal.DEBUGMODE = every_thing
+    Register.DEBUGMODE = every_thing
 
 
 def randomNBitGen(n):
@@ -10,25 +32,21 @@ def randomNBitGen(n):
         bits += str(randint(0, 1))
     return bits
 
-def bitsToGates(bitString):
-    gates = []
-    for bit in bitString:
-        if bit == "0":
-            gates.append(zero_gate.Zero())
-        elif bit == "1":
-            gates.append(one_gate.One())
-    return gates
+
+def bitsToGates(bitString, inputs):
+    for i in range(len(bitString)):
+        inputs[i].output = 0 if bitString[i] == "0" else 1
 
 
 def test1():
     clock = Signal()
-    reg1 = Register(clock, None, 32)
+    inputs = [Input(f"Input{i}") for i in range(32)]
+    reg1 = Register(clock, inputs, 32)
 
     outputs = []
     for __ in range(10):
         generated_bits = randomNBitGen(32)
-        gates = bitsToGates(generated_bits)
-        reg1.set_input(gates)
+        bitsToGates(generated_bits, inputs)
         for _ in range(1):
             print(clock.output.output)
             outputs = reg1.logic()
@@ -36,8 +54,6 @@ def test1():
         print("Generated Bits :" + generated_bits)
         print("Register Bits : " + "".join(map(str, outputs)))
 
+
+turn_off_debug()
 test1()
-
-
-
-
