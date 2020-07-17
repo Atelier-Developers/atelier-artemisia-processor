618a87ea59a1b35a41a00d5ef6e15ce612aab9b7
tavakkoliamirmohammad <tavakkoli.amirmohammad@gmail.com>
2020-07-17 13:57:57 +0430

    update main

--- Processor/main.py
+++ Processor/main.py
@@ -91,24 +91,17 @@ def test_reg_file():
     reg_file = RegisterFileUnit((read_num1, read_num2, write_num1, write_val), enable, clock, n, reg_width)
 
     for i in range(10):
+        print(i)
         print("clock :" + str(clock.output.output))
         if i % 4 == 1:
             enable.output = 1
         else:
             enable.output = 0
         print("Enable Signal: " + str(enable.output))
-        read_gen1 = randomNBitGen(size)
-        read_gen2 = randomNBitGen(size)
-        write_gen1 = randomNBitGen(size)
-        write_gen2 = randomNBitGen(reg_width)
-        print("Read num 1:" + read_gen1)
-        print("Read num 2:" + read_gen2)
-        print("Write num:" + write_gen1)
-        print("Write val:" + write_gen2)
-        bitsToGates(read_gen1, read_num1)
-        bitsToGates(read_gen2, read_num2)
-        bitsToGates(write_gen1, write_num1)
-        bitsToGates(write_gen2, write_val)
+        set_random_value(size, read_num1, "read_num1")
+        set_random_value(size, read_num2, "read_num2")
+        set_random_value(size, write_num1, "write_num")
+        set_random_value(reg_width, write_val, "write val")
         reg_file.logic()
         outputs = reg_file.get_outputs()
         # print(outputs[0])
@@ -116,7 +109,13 @@ def test_reg_file():
         print("".join(map(str, [outputs[1][i].output for i in range(reg_width)])))
         clock.pulse()
 
-        # Check Write as well
+    # Check Write as well
+
+
+def set_random_value(n, input, name):
+    read_gen = randomNBitGen(n)
+    print(f"{name}: {read_gen}")
+    bitsToGates(read_gen, input)
 
 
 turn_off_debug()
