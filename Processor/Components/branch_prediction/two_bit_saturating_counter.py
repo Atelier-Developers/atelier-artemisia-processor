3bce46e7821d3f78ed08378029f759f87416621d
amirbaghi <bamirmasoud@gmail.com>
2020-07-18 20:32:23 +0430

    fixed two bit saturating bug

--- Processor/Components/branch_prediction/two_bit_saturating_counter.py
+++ Processor/Components/branch_prediction/two_bit_saturating_counter.py
@@ -11,27 +11,29 @@ class TwoBitSaturatingCounter:
         self.clock = clock
         self.output = None
         self.d0 = None
+        self.d1 = None
         self.build()
 
     def build(self):
         d1 = D_FlipFlop(self.clock, None, f"{self.name}_d_flipflop_1")
         d0 = D_FlipFlop(self.clock, None, f"{self.name}_d_flipflop_0")
         logic_d1 = Or((
-            And((d0.output, self.input)),
-            And((d1.output, self.input)),
-            And((d0.output, d1.output))
+            And((d0, self.input)),
+            And((d1, self.input)),
+            And((d0, d1))
         ))
-        not_d0 = Not(d0.output)
+        not_d0 = Not(d0)
         logic_d0 = Or((
             And((not_d0, self.input)),
-            And((d1.output, not_d0)),
-            And((self.input, d1.output))
+            And((d1, not_d0)),
+            And((self.input, d1))
         ))
         d1.set_input(logic_d1)
         d0.set_input(logic_d0)
         d1.reset()
         d0.reset()
         self.d0 = d0
+        self.d1 = d1
         self.output = logic_d1
 
     def logic(self, depend=[]):
