76b48244d0adf3102261683813f94ac5c2513c21
tavakkoliamirmohammad <tavakkoli.amirmohammad@gmail.com>
2020-07-19 16:06:53 +0430

    add memory bank

--- Processor/Components/memory_hierarchy/memory_bank.py
+++ Processor/Components/memory_hierarchy/memory_bank.py
@@ -1,19 +1,45 @@
+from math import log
+
 from Components.memory_hierarchy.memory_cell import MemoryCell
+from decoder.decoder_mxn import Decoder_nxm
+from gate.and_gate import And
+from multiplexer.mux_mxn import Mux_mxn
 
 
 class MemoryBank:
 
-    def __init__(self, clock, inputs, size, name="MemoryBank"):
+    def __init__(self, clock, inputs, read_address, write_address, mem_write, size, name="MemoryBank"):
         self.size = size
         self.name = name
         self.inputs = inputs
         self.clock = clock
+        self.write_address = write_address
+        self.read_address = read_address
+        self.mem_write = mem_write
         self.output = None
         self.build()
 
     def build(self):
-        self.output = [MemoryCell(self.clock, self.inputs, f"{self.name}_{i}_memory_cell" for i in range(self.size)]
-
+        dec = Decoder_nxm(self.write_address, int(log(self.size, 2)))
+        mem_cells = [
+            MemoryCell(And((self.clock, dec.outputs[i], self.mem_write)), self.inputs, f"{self.name}_{i}_memory_cell")
+            for i in
+            range(self.size)]
+        self.output = [
+            Mux_mxn([mem_cells[j].output[i] for j in range(self.size)], self.read_address, int(log(self.size, 2)),
+                    f"{self.name}_mux_{i}_read")
+            for i in range(8)]
 
     def logic(self, depend=None):
-            if depend is None:
+        if depend is None:
+            depend = []
+        if self in depend:
+            return self.output
+
+        depend.append(self)
+        for mux in self.output:
+            mux.logic(depend)
+        return self.get_output()
+
+    def get_output(self):
+        return [mux.get_output() for mux in self.output]
