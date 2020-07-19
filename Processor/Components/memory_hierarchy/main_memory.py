2a58227b50ee723342062b351681fa3cf32b72e8
tavakkoliamirmohammad <tavakkoli.amirmohammad@gmail.com>
2020-07-19 16:24:46 +0430

    add main memory

--- Processor/Components/memory_hierarchy/main_memory.py
+++ Processor/Components/memory_hierarchy/main_memory.py
@@ -1,12 +1,33 @@
+from Components.memory_hierarchy.memory_bank import MemoryBank
+from itertools import chain
+
+
 class MainMemory:
 
-    def __init__(self, size, name="MainMemory"):
+    def __init__(self, clock, read_address, write_address, write_value, mem_write, mem_read, size, name="MainMemory"):
         self.size = size
         self.name = name
+        self.clock = clock
+        self.read_address = read_address  # log(size) width
+        self.write_address = write_address  # log(size) width
+        self.mem_write = mem_write
+        self.write_value = write_value  # 32bit width
+        self.mem_read = mem_read
+        self.output = None
         self.build()
 
     def build(self):
-         ####
-         ####
-         ####
-        #1024#'
+        self.output = [MemoryBank(self.clock, self.write_value[i * 8, (i + 1) * 8], self.read_address[:-2],
+                                  self.write_address[:-2], self.mem_write, self.mem_read, self.size,
+                                  f"{self.name}_mem_bank_{i}") for i in range(4)]
+
+    def logic(self, depend=None):
+        if depend is None:
+            depend = []
+        depend.append(self)
+        for bank in self.output:
+            bank.logic(depend)
+        return self.get_output()
+
+    def get_output(self):
+        return [list(chain.from_iterable(bank.get_output())) for bank in self.output]
--- Processor/Components/memory_hierarchy/memory_bank.py
+++ Processor/Components/memory_hierarchy/memory_bank.py
@@ -8,7 +8,7 @@ from multiplexer.mux_mxn import Mux_mxn
 
 class MemoryBank:
 
-    def __init__(self, clock, inputs, read_address, write_address, mem_write, size, name="MemoryBank"):
+    def __init__(self, clock, inputs, read_address, write_address, mem_write, mem_read, size, name="MemoryBank"):
         self.size = size
         self.name = name
         self.inputs = inputs
@@ -16,6 +16,7 @@ class MemoryBank:
         self.write_address = write_address
         self.read_address = read_address
         self.mem_write = mem_write
+        self.mem_read = mem_read
         self.output = None
         self.build()
 
@@ -25,10 +26,11 @@ class MemoryBank:
             MemoryCell(And((self.clock, dec.outputs[i], self.mem_write)), self.inputs, f"{self.name}_{i}_memory_cell")
             for i in
             range(self.size)]
-        self.output = [
+        muxs = [
             Mux_mxn([mem_cells[j].output[i] for j in range(self.size)], self.read_address, int(log(self.size, 2)),
                     f"{self.name}_mux_{i}_read")
             for i in range(8)]
+        self.output = [And(self.mem_read, muxs[i].output) for i in range(8)]
 
     def logic(self, depend=None):
         if depend is None:
@@ -37,9 +39,11 @@ class MemoryBank:
             return self.output
 
         depend.append(self)
+        # check for memory cell logic
         for mux in self.output:
             mux.logic(depend)
         return self.get_output()
 
     def get_output(self):
-        return [mux.get_output() for mux in self.output]
+        # ???????
+        return [block.output for block in self.output]
