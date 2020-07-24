ac18bfe24870c6d1b6b3f0d7c67f02216b708a3c
amirbaghi <bamirmasoud@gmail.com>
2020-07-25 02:04:37 +0430

    added branch forwarding unit

---
+++ Processor/Components/forwading_unit/branch_forwarding_unit.py
@@ -0,0 +1,7 @@
+class BranchForwardingUnit:
+
+    def __init__(self, rd_ex_mem, rw_ex_mem, rs_if_id, rt_if_id, rd_mem_wb, rw_mem_wb, name="BranchForwardingUnit"):
+        self.rd_ex_mem = rd_ex_mem
+        self.rw_ex_mem = rw_ex_mem
+        self.rs_if_id = rs_if_id
+        self.rt_if_id = rs_if_id
