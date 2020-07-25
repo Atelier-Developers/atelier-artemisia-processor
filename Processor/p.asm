a44f9f60382e3798acc114fb9b542776152d8d4b
amirbaghi <bamirmasoud@gmail.com>
2020-07-25 22:50:11 +0430

    modified complier a bit

--- Processor/p.asm
+++ Processor/p.asm
@@ -1,9 +1,8 @@
 addi $t2, $zero, 10
 sw $t2, 0($t2)
 lw $t3, 0($t2)
-beq $t3, $t2, end    # also negative branch should be checked
+beq $t3, $t2, -3
 lw $t3, 0($t2)
 slt $t0, $t4, $t3
 sll $s1, $t0, 3
-end:
 sub $s2, $s1, $s1
--- Processor/pipeline_test.py
+++ Processor/pipeline_test.py
@@ -14,5 +14,5 @@ def test_pipeline():
     Pipeline.run("p.asm")
 
 
-# test_compiler()
-test_pipeline()
+test_compiler()
+# test_pipeline()
